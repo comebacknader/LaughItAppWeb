e6bdf82227eeda10a53f79347ad62e2816107cf4
Nader Carun <comebacknader@gmail.com>
2021-04-04 18:34:53 -0700

    Refined test

--- src/components/__tests__/SideBar.test.tsx
+++ src/components/__tests__/SideBar.test.tsx
@@ -2,8 +2,9 @@ import * as React from 'react'
 import { render, screen } from '@testing-library/react'
 import SideBar from '../SideBar'
 
-test('the SideBar component loads', async () => {
+test('the SideBar contains a logo', async () => {
   render(<SideBar />)
-  const logo = screen.getByRole('img');
-  expect(logo).toBeTruthy()
+  const logo: HTMLImageElement | any = screen.queryByRole('img')
+  screen.debug(logo)
+  expect(logo).toBeInstanceOf(HTMLImageElement)
 })
